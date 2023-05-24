e277daace1b22da86da2a8ad4588a8ca84872669
Ahmad Malik <arma7x@live.com>
2023-05-24 11:29:31 +0800

    daily update

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-$(date)
+date=$(date +"%A, %B %C %Y")
+echo date
 #cargo run -- --latest && cp ./target/debug/pricecatcher.zip pricecatcher.zip
 git add ./ && git commit -m "daily update" #&& git push origin
